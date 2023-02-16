b4c903785b613f63e4cd1a9741427a2d34a92c8e
abhi25yenurkar <abhi25yenurkar@gmail.com>
2023-02-16 03:13:10 -0500

    Assignment no 1.

---
+++ 01_variable_assign.js
@@ -0,0 +1,31 @@
+
+console.log("************************* THIS IS STEP1 *************************");
+var myFullName;
+console.log("My Full Name is =",myFullName);
+myFullName="Abhishek s Yenurkar ";
+console.log("My Full Name is =",myFullName);
+myFullName="Abhishek Sahebrao Yenurkar";
+console.log("My Full Name is =",myFullName);
+
+console.log("************************* THIS IS STEP2 *************************");
+var myLove="I LOVE ONLY JAVASCRIPT";
+console.log(myLove);
+
+console.log("************************* THIS IS STEP3 *************************");
+
+var firstName="Abhishek";
+var lastName="Yenurkar";
+var age=24;
+var address="parbhani";
+var pincode=441118;
+var gender="male";
+var isMarried="No";
+console.log("MY Full Name is",firstName,lastName,"MY age is",age,"& Address is",address,"Pincode no-",pincode,"And The Gender is",gender," Married-",isMarried);
+
+
+
+
+
+
+
+
